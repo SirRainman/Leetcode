17c5f48420c237a3367b44b73893338a0a31125a
RainWindows <810061281@qq.com>
2020-11-07 19:18:07 +0800

    今日看的太少了

---
+++ src/main/java/dataStructure/stack/monotonousStack/A830_PreviousSmallerNum.java
@@ -0,0 +1,49 @@
+package dataStructure.stack.monotonousStack;
+
+import java.util.Scanner;
+
+/**
+ * 给定一个长度为N的整数数列，输出每个数左边第一个比它小的数，如果不存在则输出-1。
+ *
+ * 输入格式
+ * 第一行包含整数N，表示数列长度。
+ *
+ * 第二行包含N个整数，表示整数数列。
+ *
+ * 输出格式
+ * 共一行，包含N个整数，其中第i个数表示第i个数的左边第一个比它小的数，如果不存在则输出-1。
+ *
+ * 数据范围
+ * 1≤N≤105
+ * 1≤数列中元素≤109
+ * 输入样例：
+ * 5
+ * 3 4 2 7 5
+ * 输出样例：
+ * -1 3 -1 2 2
+ */
+public class A830_PreviousSmallerNum {
+
+    // TODO:单调栈，栈中只存离他最近，且最小的值。
+    //  每个元素只进栈一次，出栈一次，最多是O(2n)
+    //  1.先想暴力应该怎么做
+    //  2.再想是否有单调性在里面（把其中没有用的元素删掉）
+    //  3.优化，求极值
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] stack = new int[100010];
+        int top = -1;
+        int len = in.nextInt();
+        while(len-- > 0) {
+            int x = in.nextInt();
+            while(top > -1 && stack[top] >= x) top--;
+            if(top == -1) {
+                System.out.print("-1 ");
+            } else {
+                System.out.print(stack[top] + " ");
+            }
+            stack[++top] = x;
+        }
+    }
+}
--- src/main/java/dataStructure/stack/monotonousStack/Q496_NextGreaterElement1.java
+++ src/main/java/dataStructure/stack/monotonousStack/Q496_NextGreaterElement1.java
@@ -1,4 +1,4 @@
-package dataStructure.stack.nextBigger;
+package dataStructure.stack.monotonousStack;
 
 import java.util.Deque;
 import java.util.HashMap;
@@ -6,31 +6,27 @@ import java.util.LinkedList;
 import java.util.Map;
 
 /**
- * 给定两个 没有重复元素 的数组nums1 和nums2，其中nums1是nums2的子集。找到nums1中每个元素在nums2中的下一个比其大的值。
- *
+ * 给定两个 没有重复元素 的数组nums1 和nums2，其中nums1是nums2的子集。
+ * 找到nums1中每个元素在nums2中的下一个比其大的值。
  * nums1中数字x的下一个更大元素是指x在nums2中对应位置的右边的第一个比x大的元素。如果不存在，对应位置输出 -1 。
  *
  *
- *
  * 示例 1:
- *
  * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
  * 输出: [-1,3,-1]
  * 解释:
  *     对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
  *     对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
  *     对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
- * 示例 2:
  *
+ * 示例 2:
  * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
  * 输出: [3,-1]
  * 解释:
- *    对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
  *     对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
  *
- *
  * 提示：
- *
  * nums1和nums2中所有元素是唯一的。
  * nums1和nums2的数组大小都不超过1000。
  *
