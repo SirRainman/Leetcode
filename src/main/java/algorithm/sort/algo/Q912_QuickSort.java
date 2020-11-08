b7ee0ce1217911f6881533e0997f502555a4e5d4
SirRainman <810061281@qq.com>
2020-11-08 14:20:56 +0800

    修改了快拍的模版

--- src/main/java/algorithm/sort/algo/Q912_QuickSort.java
+++ src/main/java/algorithm/sort/algo/Q912_QuickSort.java
@@ -4,17 +4,15 @@ package algorithm.sort.algo;
  * 给你一个整数数组nums，请你将该数组升序排列。
  *
  * 示例 1：
- *
  * 输入：nums = [5,2,3,1]
  * 输出：[1,2,3,5]
- * 示例 2：
  *
+ * 示例 2：
  * 输入：nums = [5,1,1,2,0,0]
  * 输出：[0,0,1,1,2,5]
- * 
+ *
  *
  * 提示：
- *
  * 1 <= nums.length <= 50000
  * -50000 <= nums[i] <= 50000
  *
@@ -25,19 +23,23 @@ package algorithm.sort.algo;
  */
 public class Q912_QuickSort {
     public int[] sortArray(int[] nums) {
-        if(nums == null || nums.length == 1) return nums;
-        quickSort(nums, 0, nums.length-1);
+        if (nums == null || nums.length == 1) return nums;
+        quickSort(nums, 0, nums.length - 1);
         return nums;
     }
 
     // TODO:注意这种写法
     public void quickSort1(int[] nums, int left, int right) {
-        if(left >= right) return;
-        int pivot = nums[left], i = left-1, j = right+1;
-        while(i < j) {
-            do { i++; } while(nums[i] < pivot);
-            do { j--; } while (nums[j] > pivot);
-            if(i < j) {
+        if (left >= right) return;
+        int pivot = nums[left], i = left - 1, j = right + 1;
+        while (i < j) {
+            do {
+                i++;
+            } while (nums[i] < pivot);
+            do {
+                j--;
+            } while (nums[j] > pivot);
+            if (i < j) {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
@@ -45,37 +47,25 @@ public class Q912_QuickSort {
         }
         // TODO：注意为什么是j 而不是i？？？为什么会有边界问题
         quickSort(nums, left, j);
-        quickSort(nums, j+1, right);
+        quickSort(nums, j + 1, right);
     }
-
-
-
 
 
     // TODO：我的写法
     public void quickSort(int[] nums, int left, int right) {
-        if(left < right) {
-            int mid = partition(nums, left, right);
-            quickSort(nums, left, mid-1);
-            quickSort(nums, mid+1, right);
-        }
+        int mid = partition(nums, left, right);
+        quickSort(nums, left, mid - 1);
+        quickSort(nums, mid + 1, right);
     }
 
-    public int partition(int[] nums, int left, int right ) {
+    public int partition(int[] nums, int left, int right) {
+        if (left >= right) return left;
         int pivot = nums[left];
-        while(left < right) {
-            while(left < right && nums[right] >= pivot) {
-                right--;
-            }
-            if(left < right) {
-                nums[left++] = nums[right];
-            }
-            while(left < right && nums[left] <= pivot) {
-                left++;
-            }
-            if(left < right) {
-                nums[right--] = nums[left];
-            }
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) right--;
+            nums[left++] = nums[right];
+            while (left < right && nums[left] <= pivot) left++;
+            nums[right--] = nums[left];
         }
         nums[left] = pivot;
         return left;
