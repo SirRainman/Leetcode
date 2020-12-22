b785175b32564eb24dc5c89e8a61704b61a12a33
RainWindows <810061281@qq.com>
2020-12-22 12:13:02 +0800

    复习第一章

---
+++ src/main/java/algorithm/intervals/A803_MergeIntervals.java
@@ -0,0 +1,58 @@
+package algorithm.intervals;
+
+import java.util.*;
+
+/**
+ * 给定 n 个区间 [li,ri]，要求合并所有有交集的区间。
+ * 注意如果在端点处相交，也算有交集。
+ * 输出合并完成后的区间个数。
+ * 例如：[1,3]和[2,6]可以合并为一个区间[1,6]。
+ *
+ * 输入格式
+ * 第一行包含整数n。
+ * 接下来n行，每行包含两个整数 l 和 r。
+ *
+ * 输出格式
+ * 共一行，包含一个整数，表示合并区间完成后的区间个数。
+ *
+ * 数据范围
+ * 1≤n≤100000,
+ * −109≤li≤ri≤109
+ * 输入样例：
+ * 5
+ * 1 2
+ * 2 4
+ * 5 6
+ * 7 8
+ * 7 9
+ * 输出样例：
+ * 3
+ *
+ * https://www.acwing.com/problem/content/805/
+ */
+public class A803_MergeIntervals {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int len = in.nextInt();
+        int[][] intervals = new int[len][2];
+        for(int i = 0; i < len; i++) {
+            intervals[i][0] = in.nextInt();
+            intervals[i][1] = in.nextInt();
+        }
+
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        for(int i = 0; i < len; i++) {
+            int start = intervals[i][0], end = intervals[i][1];
+            int currentSize = mergedIntervals.size();
+            if(currentSize == 0 || mergedIntervals.get(currentSize - 1)[1] < start) { // 没有交集
+                mergedIntervals.add(new int[]{start, end});
+            } else { // 更新端点
+                mergedIntervals.get(currentSize - 1)[1] = Math.max(end, mergedIntervals.get(currentSize - 1)[1]);
+            }
+        }
+
+        System.out.print(mergedIntervals.size());
+    }
+}
